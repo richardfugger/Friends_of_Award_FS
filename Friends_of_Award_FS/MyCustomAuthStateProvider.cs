2e7291ca93bfb41656fadfaf888b0634e4797fa6
richardfugger <richard.fugger@gmail.com>
2025-12-02 11:09:49 +0100

    Implementierung AuthorizedView

---
+++ Friends_of_Award_FS/MyCustomAuthStateProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Blazor_WithAuth_ForSWP5
+{
+
+	public class MyCustomAuthStateProvider : AuthenticationStateProvider
+	{
+		private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+		// _anonymous - falls _currentUser null ist
+		private ClaimsPrincipal? _currentUser = null;
+
+
+		public override Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
+		}
+
+
+		public void Login(string username)
+		{
+			ClaimsIdentity identity = new([new Claim(ClaimTypes.Name, username)],
+					"MyCustomAuthType");  // von SPAA erfunden, keiner der Standardtypen
+
+			_currentUser = new ClaimsPrincipal(identity);
+			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+		}
+
+
+		public void Logout()
+		{
+			_currentUser = null;
+			NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+		}
+	}
+}
--- Friends_of_Award_FS/Program.cs
+++ Friends_of_Award_FS/Program.cs
@@ -1,10 +1,18 @@
+using Blazor_WithAuth_ForSWP5;
 using Friends_of_Award_FS.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<MyCustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider =>
+  provider.GetRequiredService<MyCustomAuthStateProvider>());
 
 var app = builder.Build();
 
