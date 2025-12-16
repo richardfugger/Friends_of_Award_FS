7944ffaede347352acf4937fe8e6a4a9471503f2
richardfugger <richard.fugger@gmail.com>
2025-12-16 11:24:04 +0100

    QR-Code: -Generation - DB-Integration

--- Friends_of_Award_FS_Lib/Token.cs
+++ Friends_of_Award_FS_Lib/Token.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using static QRCoder.PayloadGenerator;
 
 namespace Friends_of_Award_FS_Lib
 {
@@ -67,6 +68,55 @@ namespace Friends_of_Award_FS_Lib
             try
             {
                 string sql = $"INSERT INTO foa_qr_tokens (token) VALUES ('{token}')";
+
+                int numRows = wrappr.RunNonQuery(sql);
+                if (numRows != 1) success = false;
+                else success = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                success = false;
+            }
+
+            return success;
+        }
+
+        public static List<string> LoadTokenFromDb()
+        {
+            DbWrapperMySqlV2 wrappr = DbWrapperMySqlV2.Wrapper;
+            DataTable dt;
+            List<string> unusedTokenList = new();
+
+            try
+            {
+                string sql = $"SELECT token FROM foa_qr_tokens WHERE used = 0";
+                dt = wrappr.RunQuery(sql);
+                if (dt != null)
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        unusedTokenList.Add(dr[0].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                unusedTokenList = new();
+            }
+
+            return unusedTokenList;
+        }
+
+        public static bool MarkAsUsed(string token)
+        {
+            bool success = false;
+            DbWrapperMySqlV2 wrappr = DbWrapperMySqlV2.Wrapper;
+
+            try
+            {
+                string sql = "UPDATE foa_qr_tokens SET used = 1 WHERE token = @token";
 
                 int numRows = wrappr.RunNonQuery(sql);
                 if (numRows != 1) success = false;
