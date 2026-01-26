04f2ee40d8dcab0f46b947ebacd25ef405dacd9e
richardfugger <richard.fugger@gmail.com>
2026-01-26 09:18:49 +0100

    Start Admin Pages design

--- Friends_of_Award_FS/Program.cs
+++ Friends_of_Award_FS/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddScoped<DiplomarbeitenExportService>();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
